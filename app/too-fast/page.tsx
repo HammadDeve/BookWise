2252a063b5d2d2c1a8cecd43162c4aed8768d831
Muhammad Hammad <hammadiftikhar3596@gmail.com>
2025-09-07 11:43:11 +0500

    First commit

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="root-container flex min-h-screen flex-col items-center justify-center">
+      <h1 className="font-bebas-neue text-5xl font-bold text-light-100">
+        "whoa, Slow Down There, Speedy!
+      </h1>
+      <p className="mt-3 max-w-xl text-center text-light-400">
+        😅 Oops! Looks like you've been a little too eager 🚀 We've put a
+        temporary pause ⏸️ on your excitement. Chill 🧊 for a bit, and try again
+        🔄
+      </p>
+    </main>
+  );
+};
+
+export default page;
---
+++ components/ImageUpload.tsx
@@ -0,0 +1,113 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import ImageKit from "imagekit";
+import config from "@/lib/config";
+import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
+import { useRef } from "react";
+import { Button } from "./ui/button";
+import { toast } from "@/hooks/use-toast";
+import { title } from "process";
+
+const authenticator = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
+    }
+
+    const data = await response.json();
+
+    const { signature, expire, token } = data;
+
+    return { token, expire, signature };
+  } catch (error: any) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
+};
+
+const {
+  env: {
+    imagekit: { publicKey, urlEndpoint },
+  },
+} = config;
+
+const ImageUpload = ({
+  onFileChange,
+}: {
+  onFileChange: (filePath: string) => void;
+}) => {
+  const IKUploadRef = useRef(null);
+  const [file, setFile] = useState<{ filePath: string } | null>(null);
+  const onError = (error: any) => {
+    console.log(error);
+
+    toast({
+      title: "Image uplaoded Failed",
+      description: "Your image cannot be uplaoded. Please try again",
+      variant: "destructive",
+    });
+  };
+  const onSuccess = (res: any) => {
+    setFile(res);
+    onFileChange(res.filePath);
+
+    toast({
+      title: "Image uplaoded Successfuly",
+      description: `${res.filePath} uploaded succcessfuly`,
+    });
+  };
+  return (
+    <ImageKitProvider
+      publicKey={publicKey}
+      urlEndpoint={urlEndpoint}
+      authenticator={authenticator}
+    >
+      <IKUpload
+        className="hidden"
+        ref={IKUploadRef}
+        onError={onError}
+        onSuccess={onSuccess}
+        fileName="test-upload.png"
+      />
+
+      <button
+        className="upload-btn"
+        onClick={(e) => {
+          e.preventDefault();
+
+          if (IKUploadRef.current) {
+            // @ts-ignore
+            IKUploadRef.current?.click();
+          }
+        }}
+      >
+        <Image
+          src="/icons/upload.svg"
+          alt="upload-icon"
+          width={20}
+          height={20}
+          className="object-contain"
+        />
+        <p className="text-base text-light-100">Upload a File</p>
+        {file && <p className="upload-filename">{file.filePath}</p>}
+      </button>
+
+      {file && (
+        <IKImage
+          alt={file.filePath}
+          path={file.filePath}
+          width={500}
+          height={500}
+        />
+      )}
+    </ImageKitProvider>
+  );
+};
+
+export default ImageUpload;
